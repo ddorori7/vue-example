7aedb82de67bd9c1f46cc683c29079242f806fcc
서지원 <qrd7079@gmail.com>
2021-09-25 21:10:41 +0900

    부모 컴포넌트로 데이터 보내기(emit)

---
+++ test1/src/components/InputField.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label for="">Name</label>
+    <!-- <input
+      type="text"
+      :value="name"
+      style="padding: 30px; border: 2px solid
+    red;"
+      @input="updateName"
+    /> -->
+    <input
+      type="text"
+      :value="name"
+      style="padding: 30px; border: 2px solid
+    red;"
+      @input="$emit('update-name', $event)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  // methods: {
+  //  updateName(e) {
+  //    console.log(e.target.value);
+  //    // 부모 컴포넌트로 보내는 방법
+  //    //   this.$emit("원하는 이벤트 이름", 보내려는 값);
+  //    this.$emit("update-name", e.target.value);
+  // },
+  //  },
+};
+</script>
+
+<style></style>
--- test1/src/views/Home.vue
+++ test1/src/views/Home.vue
@@ -6,22 +6,39 @@
     <p>더 만들고 싶으면 더 쪼개서 components폴더에!</p>
     <p>컴포넌트 불러와서 사용</p>
     <!-- jiwon.vue에서 props로 보낸 타이틀을 속성으로 사용! -->
-    <jiwon name="ddorori7" /></div
-></template>
+    <jiwon name="ddorori7" />
+
+    <h2>부모 컴포넌트로 데이터 보내기</h2>
+    <form action="">
+      <!-- <InputField :name="name" @update-name="updateName" /> -->
+      <InputField :name="name" @update-name="name = $event.target.value" />
+      <br />
+      <button>submit</button>
+    </form>
+    {{ name }}
+  </div>
+</template>
 
 <script>
 // 컴포넌트 불러와서 사용!
 // @는 src 소스를 가리킴
 import jiwon from "@/components/jiwon.vue";
+import InputField from "@/components/InputField.vue";
 
 export default {
   components: {
     jiwon,
+    InputField,
   },
   data() {
     return {
-      name: "jiwon",
+      name: "",
     };
+  },
+  methods: {
+    updateName(name) {
+      this.name = name;
+    },
   },
 };
 </script>
